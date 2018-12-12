58a77c69cda24b845dbea02889e6423e2f454b2c
gjb <543436798@qq.com>
2018-12-12 21:15:37 +0800

    完成aop expression的解析

--- src/main/java/aop/PointCut.java
+++ src/main/java/aop/PointCut.java
@@ -1,13 +1,61 @@
 package aop;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class PointCut {
 public String expression;
+public PointCut(String expression) {
+	this.expression = expression;
+}
 public boolean isMatch(Object targetObject,Method method) {
-	return true;
+    return pathMatch(targetObject, method);
 }
 public boolean isMatch(Object targetObject) {
-	return true;
+	return pathMatch(targetObject,null);
+}
+public boolean regexMatch(String regex,String input) {
+	regex=regex.replace("*", ".*");
+	Pattern pattern=Pattern.compile(regex);
+	return pattern.matcher(input).matches();
+}
+public boolean pathMatch(Object targetObject,Method method) {
+	String scope=expression.split(" ")[0].replace("execution(", "").trim();//解析表达式范围
+	String target=expression.split("\\(")[1].split(" ")[1];//解析表达式路径
+	String[]splitTarget=target.split("\\.");
+	List<String> regexList=new ArrayList<String>();
+	for(int i=0;i<splitTarget.length;i++) {//处理正则表达式
+		if(splitTarget[i].equals("")) {
+			String regex=regexList.get(regexList.size()-1);
+			regex+="|"+splitTarget[++i];
+			regexList.remove(regexList.size()-1);
+			regexList.add(regex);
+		}else {
+			regexList.add(splitTarget[i]);
+		}
+	}
+	//开始判断路径是否匹配
+	String beanClass=targetObject.getClass().getName();//获取beanClass名字
+	String[] splitClass=beanClass.split("\\.");
+	boolean pathCheck=false;
+	int index=0;
+	for(;index<regexList.size()-1;index++) {
+		if(regexList.get(index).equals("*")) {
+			pathCheck=true;
+			break;
+		}
+		if(regexMatch(regexList.get(index), splitClass[index])) {
+			continue;
+		}
+		break;
+	}
+	pathCheck=(index==regexList.size()-1)&&(index==splitClass.length);
+	if(method!=null) {
+		return regexMatch(regexList.get(regexList.size()-1), method.getName())&&pathCheck;
+	}else {
+		return pathCheck;
+	}
 }
 }
--- src/main/java/nullguo/winter/App.java
+++ src/main/java/nullguo/winter/App.java
@@ -1,8 +1,6 @@
 package nullguo.winter;
 
-import com.alibaba.fastjson.JSON;
 
-import Factory.BeanDefinition;
 import Factory.WinterFactory;
 import aop.Advice;
 import aop.Advisor;
@@ -18,10 +16,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         WinterFactory.parse("C:/Users/Administrator/eclipse-workspace/winter/src/main/java/winter.xml");
-        Advisor advisor=new Advisor(new PointCut(),new Advice("around",new AopTest(),"before"));
+        Advisor advisor=new Advisor(new PointCut("execution(* nullguo.winter.Test.toString(..))"),new Advice("around",new AopTest(),"before"));
         WinterFactory.setSingletonBean("aopTest", advisor);
-        Advisor advisor1=new Advisor(new PointCut(),new Advice("afterReturning",new AopTest(),"before"));
-        WinterFactory.setSingletonBean("aopTest1", advisor1);
         WinterFactory.initBeans();
         System.out.println(WinterFactory.getSingletonBean("test1"));
         System.out.println(WinterFactory.getSingletonBean("test3"));
